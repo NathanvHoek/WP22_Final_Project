1468f149433c0aae29394224bd41423b861b2893
Claerson <t.ballout@outlook.com>
2022-06-24 23:20:50 +0200

    testing the animation for flipping cards

---
+++ js/card_flip.js
@@ -0,0 +1,8 @@
+function flipCard() {
+    var cards = document.querySelectorAll('.card');
+    [...cards].forEach((card) => {
+        card.addEventListener('click', function () {
+            card.classList.toggle('is-flipped');
+        });
+    });
+}
