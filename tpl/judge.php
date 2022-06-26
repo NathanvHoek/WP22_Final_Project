69c979742b03002854af1b02dde75169c43c607a
Simon van Loon <s.p.c.a.van.loon@student.rug.nl>
2022-06-26 15:57:14 +0200

    stijl van judge.php veranderd.

--- tpl/judge.php
+++ tpl/judge.php
@@ -1,6 +1,31 @@
-<?php include "header.php" ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-left">
+                <li ><a href="../index_test.php">HOME</a></li>
+                <li><a href="#services">ABOUT US</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 <div class="container">
+
+
 
     <div class="head">
         <h1>Hey, <?= $player_name ?>, you are the judge!</h1>
