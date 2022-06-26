2d1a15bb7c0b9c8e3943e7addf98c32bca05886d
Simon van Loon <s.p.c.a.van.loon@student.rug.nl>
2022-06-26 19:57:32 +0200

    verder gegaan met footer.

--- about.php
+++ about.php
@@ -103,8 +103,19 @@
                     </ul>
                 </div>
             </div>
+
             <!-- End-->
 
         </div>
     </div>
+
+</div>
+<div class="container">
+    <footer class="py-3 my-4">
+        <ul class="nav text-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="index_test.php" class="nav-link px-2 text-muted">Home</a></li>
+            <li class="nav-item"><a href="about.php" class="nav-link px-2 text-muted">About</a></li>
+        </ul>
+        <p class="text-center text-muted">© 2022 Copyright:Tariq Ballout, Nathan van Hoek, Simon van Loon</p>
+    </footer>
 </div>
